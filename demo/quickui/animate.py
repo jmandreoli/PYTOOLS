f08e6b1da22a3cd4eb203edfeb88a37707c5b6d9
Jean-Marc Andreoli <jean-marc.andreoli@xrce.xerox.com>
2015-07-11 12:08:54 +0200

    Updates

---
+++ demo/quickui/animate.py
@@ -0,0 +1,135 @@
+from functools import partial
+
+#--------------------------------------------------------------------------------------------------
+def matplotlibcfg():
+  """Configurations for matplotlib animations"""
+#--------------------------------------------------------------------------------------------------
+  from matplotlib.animation import FuncAnimation
+  del FuncAnimation.new_saved_frame_seq # fixes a bug
+  from matplotlib import rcParams
+  rcParams['animation.mencoder_path'] = '/opt/Linux/bin/mencoder'
+matplotlibcfg()
+del matplotlibcfg
+
+#--------------------------------------------------------------------------------------------------
+def launch(syst=None,animate=None,axes=(lambda fig: fig.add_subplot(1,1,1)),**ka):
+#--------------------------------------------------------------------------------------------------
+    from matplotlib.pyplot import figure, show
+    from matplotlib.animation import FuncAnimation
+    fig = figure()
+    syst.display(axes(fig),animate=partial(FuncAnimation,**animate),**ka)
+    show()
+
+#--------------------------------------------------------------------------------------------------
+def launchui(cfg,axes=(lambda view: view.make_axes())):
+#--------------------------------------------------------------------------------------------------
+  from myutil import quickui, mplext
+
+  class MyExperimentUI (quickui.ExperimentUI):
+
+    def setup(self):
+      Q = quickui.cbook
+      view = Q.mplview(self,'main')
+      timer = Q.mpltimer(view.figure)
+      main = lambda view,syst=None,**ka: syst.display(axes(view),**ka)
+      def exper(ka):
+        timer.shutdown()
+        with view.clearm(): main(view,**ka)
+      super(MyExperimentUI,self).setup(cfg(timer),exper)
+
+  with quickui.startup(): MyExperimentUI()
+
+#--------------------------------------------------------------------------------------------------
+def cfg_anim(basetimer,modifier={}):
+#--------------------------------------------------------------------------------------------------
+  p = pdict(
+    repeat_delay=0,
+    timer=pdict(
+      paused=True,
+      interval=300,
+      ),
+    save=pdict(
+      fps=5,
+      writer='mencoder',
+      extra_args='',
+      metadata=pdict(
+        title='',
+        artist='Jean-Marc Andreoli <jean.marc.andreoli@xrce.xerox.com>',
+        ),
+      )
+    )
+  p.update(**modifier)
+  from myutil import quickui
+  Q = quickui.cbook
+  return (
+    Q.Cclosr(partial(animate,basetimer,**p),multi=False),
+    ('timer',dict(sel=True),
+     Q.Ctransf(partial(dict,**p._.timer)),
+     ('interval',dict(accept=Q.i,tooltip='inter-frame interval in ms'),
+      Q.Cbase.LinScalar(step=1,vmin=10,vmax=1000,vtype=int)
+      ),
+     ('paused',dict(accept=Q.i,tooltip='whether the animation starts as paused'),
+      Q.Cbase.Boolean()
+      ),
+     ),
+    ('save',dict(sel=False,anchor=True),
+     Q.Ctransf(partial(dict,**p._.save),multi=True),
+     ('fps',dict(accept=Q.i,sel=False,tooltip='frame per seconds'),
+      Q.Cbase.LinScalar(step=1,vmin=1,vmax=50,vtype=int)
+      ),
+     ('filename',dict(accept=Q.i,sel=None),
+      Q.Cbase.Filename(op='save',caption='Save animation',filter='mpeg4 (*.mp4)')
+      ),
+     ('extra_args',dict(accept=Q.i,sel=False,tooltip='tuple of strings passed to writer command line'),
+      Q.Cbase.Object(initv=(),transf=(lambda s:s.strip().split()),itransf=(lambda x:' '.join(x)),fmt=(lambda x: ' '.join(x)),editor_location='bottom')
+      ),
+     ('metadata',dict(),
+      Q.Ctransf(partial(dict,**p._.save._.metadata)),
+      ('artist',dict(accept=Q.i),
+       c_str()
+       ),
+      ('title',dict(accept=Q.i),
+       c_str()
+       ),
+      ),
+     ),
+    ('repeat_delay',dict(accept=Q.i,sel=None,tooltip='delay before next iteration of animation loop in ms (no looping behaviour if null)'),
+     Q.Cbase.LinScalar(step=1000,vmin=0,vmax=10000,vtype=int)
+     ),
+    )
+
+#--------------------------------------------------------------------------------------------------
+# Utilities
+#--------------------------------------------------------------------------------------------------
+
+def c_str():
+  from myutil import quickui
+  Q = quickui.cbook
+  return Q.Cbase.Object(initv='',transf=(lambda s:s),itransf=(lambda s:s),editor_location='bottom')
+
+def animate(basetimer,fig,timer=None,save=None,**ka):
+  ka['repeat'] = ka['repeat_delay']>0
+  from matplotlib.animation import FuncAnimation
+  anim = FuncAnimation(fig,save_count=1,**ka)
+  if timer is not None:
+    config(basetimer,**timer)
+    basetimer.launch(anim)
+  if save is not None: anim.save(**save)
+
+def config(x,**ka):
+  for k,v in ka.items(): setattr(x,k,v)
+  return x
+
+class pdict (dict):
+  class obj(object): pass
+  def __init__(self,**kwd):
+    super(pdict,self).__init__((k,v) for k,v in kwd.items() if not isinstance(v,pdict))
+    self._ = config(self.obj(),**dict((k,v) for k,v in kwd.items() if isinstance(v,pdict)))
+  def update(self,**d):
+    if not d: return
+    for k,v in self._.__dict__.items():
+      try: dd = d.pop(k)
+      except: pass
+      else: v.update(**dd)
+    if not d: return
+    super(pdict,self).update(**d)
---
+++ demo/quickui/randwalk.py
@@ -0,0 +1,146 @@
+import logging
+logger = logging.getLogger(__name__)
+
+from numpy import array, zeros, ones, empty, arange, newaxis, abs, sum, square, sqrt, log, exp, argmin, argmax, amin, amax, nonzero, all, any, nan, isnan, dot, mean, average, std
+from numpy import concatenate, cumsum, maximum, infty, ceil, where
+from numpy.random import multivariate_normal
+from functools import partial
+
+#==================================================================================================
+class RandomWalk (object):
+#==================================================================================================
+
+    def __init__(self,stepg,N):
+        assert isinstance(N,int)
+        trip = stepg(N)
+        trip = concatenate((zeros((1,trip.shape[1])),trip),axis=0)
+        self.trip = cumsum(trip,axis=0)
+        self.norm = sqrt(sum(square(self.trip),axis=1))
+
+    def display(self,axes,animate=None):
+        ax,axnorm = axes
+        N = len(self.trip)
+        ax.set_aspect('equal')
+        ax.set_title('A normal random walk')
+        ax.scatter(0.,0.,c='r',zorder=1)
+        a_trip = ax.plot((),(),'b')[0]
+        a_end = ax.scatter(0.,0.,c='r',marker='^',zorder=1)
+        axnorm.set_title('Distance to the origin')
+        time = arange(N)
+        a_norm = axnorm.plot((),(),c='k')[0]
+        clock = axnorm.text(.05,.95,'*',ha='center',transform=ax.transAxes,bbox=dict(edgecolor='k',facecolor='none'))
+        btrip = arrround(abs(self.trip))
+        bnorm = arrround(self.norm)
+        def disp(t):
+            a_trip.set_data(self.trip[:t+1,0],self.trip[:t+1,1])
+            a_end.set_offsets((self.trip[t,0],self.trip[t,1]))
+            a_norm.set_data(time[:t+1],self.norm[:t+1])
+            b = btrip[t]
+            if t==0 or any(btrip[t-1]<b): ax.set_xlim(-b[0],b[0]); ax.set_ylim(-b[1],b[1])
+            if t%20==0: axnorm.set_xlim(0,t+20)
+            b = bnorm[t]
+            if t==0 or bnorm[t-1]<b: axnorm.set_ylim(0,b)
+            clock.set_text(str(t))
+        if animate is None: disp(N-1)
+        else: return animate(ax.figure,func=disp,init_func=(lambda: ()),frames=N)
+
+#==================================================================================================
+# Utilities
+#==================================================================================================
+
+def bvn(vx=1.,vy=1.,cor=0.,mx=0.,my=0.):
+    vxy=cor*sqrt(vx*vy)
+    mean = array((mx,my))
+    cov = array(((vx,vxy),(vxy,vy)))
+    return lambda n, g=multivariate_normal: g(mean,cov,(n,))
+
+def arrround(a,axis=0,r=5.,s=log(1.5)):
+    a = maximum.accumulate(a,axis=axis)
+    m = amin(where(a!=0,a,infty),axis=axis)
+    a = where(a!=0,a,m)
+    return r*exp(ceil(log(a/r)/s)*s)
+
+#--------------------------------------------------------------------------------------------------
+def run():
+#--------------------------------------------------------------------------------------------------
+    from animate import launch
+    syst = RandomWalk(
+        stepg=bvn(vx=3.1,vy=2.8,cor=.5),
+        N=400,
+        )
+    animate=dict(
+        repeat=False,
+        interval=50,
+        )
+    def axes(fig):
+        fig.set_size_inches((8,12),forward=True)
+        return fig.add_subplot(2,1,1),fig.add_subplot(2,1,2)
+    launch(syst,animate,axes=axes)
+
+#--------------------------------------------------------------------------------------------------
+def runui():
+#--------------------------------------------------------------------------------------------------
+    from animate import launchui
+    from myutil import set_qtbinding; set_qtbinding('pyqt4')  
+    def config(*animate):
+        from animate import cfg_anim
+        from myutil import quickui
+        ffmt = '{0:.3f}'.format
+        Q = quickui.cbook
+        return quickui.configuration(
+            Q.Ctransf(dict,multi=True),
+            ('syst',dict(sel=None),
+             Q.Ctransf(
+                partial(
+                  RandomWalk,
+                  N=400,
+                  )
+                ),
+             ('stepg',dict(tooltip='Specification of the normal 0-mean random step'),
+              Q.Ctransf(
+                  partial(
+                    bvn,
+                    vx=3.1,
+                    vy=2.8,
+                    cor=.5
+                    )
+                  ),
+              ('vx',dict(accept=Q.i),
+               Q.Cbase.LinScalar(vmin=.1,vmax=1.,nval=1000,vtype=float,fmt=ffmt)
+               ),
+              ('vy',dict(accept=Q.i),
+               Q.Cbase.LinScalar(vmin=.1,vmax=1.,nval=1000,vtype=float,fmt=ffmt)
+               ),
+              ('cor',dict(accept=Q.i),
+               Q.Cbase.LinScalar(vmin=0.,vmax=1.,nval=1000,vtype=float,fmt=ffmt)
+               )
+              ),
+             ('N',dict(accept=Q.i),
+              Q.Cbase.LinScalar(step=1,vmin=100,vmax=1000,vtype=int)
+              ),
+             ),
+            ('animate',dict(sel=False))+
+            cfg_anim(
+              *animate,
+               modifier=dict(
+                save=dict(
+                  filename='randwalk.mp4',
+                  metadata=dict(
+                    title='A random walk',
+                    ),
+                  ),
+                )
+               ),
+            )
+    def axes(view):
+        view.figure.set_size_inches((8,12),forward=True)
+        view.make_grid(2,1)
+        return view[0,0].make_axes(),view[1,0].make_axes()
+    launchui(config,axes=axes)
+
+#--------------------------------------------------------------------------------------------------
+
+if __name__=='__main__':
+    logging.basicConfig(level=logging.INFO)
+    runui()
+
